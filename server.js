0dd12396db3e439022d55501f8d133eb49a39cd7
Androsov Anrii <a.androsov@samsung.com>
2020-01-13 13:46:34 +0200

    UPDATED

--- server.js
+++ server.js
@@ -1,20 +1,37 @@
 // values for the enviroment variables set in the .env file can be accesed at proces.env.VARIABLE_NAME
+const fs = require('fs');
+const jwt = require('jsonwebtoken');
+var privateKEY  = fs.readFileSync('.data/private-key.pem', 'utf8');
+/*
+ ====================   JWT Signing =====================
+*/
+var payload = {
+ data1: "Data 1",
+ data2: "Data 2",
+ data3: "Data 3",
+ data4: "Data 4",
+};
+var signOptions = {
+ expiresIn:  "12h",
+ algorithm:  "RS256"   // RSASSA [ "RS256", "RS384", "RS512" ]
+};
+
+
 var express = require('express')
 var app1 = express()
 app1.use(express.json())
 app1.set('port', (process.env.PORT || 3000))
 
 const app = require('github-app')({
-  id: process.env.APP_ID,
+  id: process.env.APP_ID || 50724,
   cert: require('fs').readFileSync('.data/private-key.pem')
 })
 
-const secret = process.env.WEBHOOK_SECRET
+const secret = process.env.WEBHOOK_SECRET || "1234567890"
 const webHookHandler = require('github-webhook-handler')({
   path: '/',
   secret: secret
 })
-
 
 webHookHandler.on('issues', (event) => {
   // ignore all issue events other than new issue opened
@@ -31,27 +48,12 @@ webHookHandler.on('issues', (event) => {
   })
 })
 
-webHookHandler.on('pull_request', (event) => {
+webHookHandler.on('pull_request.opened', (event) => {
   console.log("PULL REQUEST EVENT")
   console.log(event.payload)
   // ignore all issue events other than new issue opened
   //if (event.payload.action !== 'opened') return
-  const {installation, repository, issue} = event.payload
-  app.asInstallation(installation.id).then((github) => {
-    github.pullRequests.createComment({
-      owner: repository.owner.login,
-      repo: repository.name,
-      number: issue.number,
-      body: 'Welcome: AVAS just started analysis of your repository for pull request'
-    })
-  })
 })
-
-webHookHandler.on("*", (event) => {
-  console.log("event received");
-  console.log(event.payload);
-  console.log("event received");
-});
 
 webHookHandler.on('push', (event) => {
   console.log("PUSH EVENT")
@@ -61,16 +63,19 @@ webHookHandler.on('push', (event) => {
 
 app1.get('/', function(request, response) {
   response.send('Hello World!')
+  var token = jwt.sign(payload, privateKEY, signOptions);
+  console.log("Token :" + token);
 })
 
-/*app1.post('/', function(request, response) {
+app1.post('/', function(request, response) {
   webHookHandler(request, response, () => response.end('ok'))
-})*/
-
-app1.post('/webhook', function(request, response) {
-  console.log(request.body);
-  response.send('Hello World!')
 })
+
+/*app1.post('/webhook', function(request, response) {
+  console.log(request.body);
+  console.log(request.body.pull_request.head.sha);
+  response.send('Hello World!');
+})*/
 
 function handleRequest (request, response) {
   // ignore all requests that aren’t POST requests
@@ -112,6 +117,8 @@ app1.listen(app1.get('port'), function() {
 //     response.end('ok')
 //   })
 // }
+
+
 
 
 
